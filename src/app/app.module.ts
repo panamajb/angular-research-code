c5d97e189575d0fbad57b7c76c1c82e4ff131df4
James Beattie <panamajb@gmail.com>
2020-09-06 13:45:30 -0400

    uodated module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+//
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FlexLayoutComponent} from './arc-components/flex-layout/flex-layout.component';
+import {ObservablesComponent} from './arc-components/observables/observables.component';
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlexLayoutComponent,
+    ObservablesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
