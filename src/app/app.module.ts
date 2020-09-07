3132ea9aa09195f0545121a8e538b132b9c9af82
James B <panamajb@gmail.com>
2020-09-06 21:08:04 -0400

    jwt-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import {FlexLayoutComponent} from './arc-components/flex-layout/flex-layout.component';
 import {ObservablesComponent} from './arc-components/observables/observables.component';
 import {FlexLayoutModule} from "@angular/flex-layout";
+import {JwtModule} from '@auth0/angular-jwt';
+
+export function tokenGetterFn() {  return localStorage.getItem('token');}
 
 @NgModule({
   declarations: [
@@ -16,7 +19,12 @@ import {FlexLayoutModule} from "@angular/flex-layout";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetterFn
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/arc-services/auth-service.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+
+import { JwtHelperService} from '@auth0/angular-jwt';
+import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router';
+import {Credential} from '../arc-models/credential.model';
+import {User} from '../arc-models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthServiceService {
+  private BASE_URL = 'http://localhost:8081/ims-users/resources';
+
+  private user: User;
+
+  constructor(private jwtHelper: JwtHelperService,
+              private http: HttpClient,
+              private router: Router) {
+  }
+
+  public getauthenticated(): boolean {
+    const token = this.jwtHelper.tokenGetter();
+    if (token) {
+      return !this.jwtHelper.isTokenExpired(token);
+    }
+    return false;
+  }
+
+  public login(userCreds: Credential) {
+    let url: string = `${this.BASE_URL}/users/authenticate`;
+    return this.http.post(url, userCreds,
+      { responseType: 'text' }
+    ).subscribe(tokenResult => {
+      localStorage.setItem('token', tokenResult);
+      this.user = new User();
+      this.user.name = userCreds.username;
+
+      this.router.navigate(['issues']);
+    });
+  }
+  /* Logout in AuthService simply removes token */
+  public logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['login']);
+  }
+
+  public get currentUser(): User {
+    return this.user;
+  }
+
+
+
+
+}
